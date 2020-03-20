d22148fd5ae129144409112cd08b7fd0e3cf6ec5
sergivna00 <sergivna00@gmail.com>
2020-03-20 21:08:45 +0200

    add methods Eat, TakeFork, ReturnFork

--- DiningPhilosophers/Fork.cs
+++ DiningPhilosophers/Fork.cs
@@ -7,5 +7,28 @@ namespace DiningPhilosophers
     class Fork
     {
         int number;
+        public bool isFree { get; private set; }
+
+        public bool TakeFork()
+        {
+            if(isFree)
+            {
+                isFree = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool ReturnFork()
+        {
+            if(!isFree)
+            {
+                isFree = true;
+                return true;
+            }
+
+            return false;
+        }
     }
 }
--- DiningPhilosophers/Philosopher.cs
+++ DiningPhilosophers/Philosopher.cs
@@ -1,23 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DiningPhilosophers
 {
     class Philosopher
     {
-        int number;
+        int Number { get;}
         Fork left;
         Fork right;
 
-        public Philosopher(int number)
+        bool hasLeft;
+        bool hasRight;
+
+        Thread thread;
+
+        public Philosopher(int number, Fork left, Fork right)
         {
-            this.number = number;
+            this.Number = number;
+            this.left = left;
+            this.right = right;
         }
 
         public void Eat()
         {
-            throw new NotImplementedException();
+            hasLeft = left.TakeFork();
+            hasRight = right.TakeFork();
+            
+            if(hasLeft && hasRight)
+            {
+                Console.WriteLine($"Philosopher {Number} is eating");
+                Thread.Sleep(20);
+            }
+
+            hasLeft = left.ReturnFork();
+            hasRight = right.ReturnFork();
+
+            Console.WriteLine("Philosopher finished");
         }
     }
 }
