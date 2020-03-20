68774a8efbfa22915979139b4861342f440dcbd1
unknown <YuliiaProkopenko@epam.com>
2020-03-20 20:33:35 +0200

    add classes Philosopher and Fork

---
+++ DiningPhilosophers/Philosopher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiningPhilosophers
+{
+    class Philosopher
+    {
+        int number;
+        Fork left;
+        Fork right;
+
+        public Philosopher(int number)
+        {
+            this.number = number;
+        }
+
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
